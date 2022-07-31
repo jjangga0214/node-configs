dddb5e517b782e8857aef41d554febbfbd5eaf91
jjangga0214 <bnbcmindnpass@gmail.com>
2022-07-31 22:36:44 +0900

    chore(commitlint-config): v1.0.8

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -8,4 +8,10 @@ module.exports = {
     'footer-max-line-length': [0, 'always'],
     'subject-case': [0, 'always'],
   },
+  prompt: {
+    settings: {
+      enableMultipleScopes: true,
+      scopeEnumSeparator: ','
+    }
+  },
 }
